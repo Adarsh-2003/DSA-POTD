d203fa7fa189b392ecb90ea423f5eefcd4877799
Adarsh Gupta <adgupta183@gmail.com>
2023-10-24 10:56:07 +0530

    oct24

---
+++ LeetCode/October-2023/oct24.java
@@ -0,0 +1,46 @@
+/*
+515. Find Largest Value in Each Tree Row
+
+Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+
+Example 1:
+
+
+Input: root = [1,3,2,5,3,null,9]
+Output: [1,3,9]
+Example 2:
+
+Input: root = [1,2,3]
+Output: [1,3]
+ 
+
+Constraints:
+
+The number of nodes in the tree will be in the range [0, 104].
+-231 <= Node.val <= 231 - 1
+*/
+
+class Solution {
+  public List<Integer> largestValues(TreeNode root) {
+    if (root == null)
+      return new ArrayList<>();
+
+    List<Integer> ans = new ArrayList<>();
+    Queue<TreeNode> q = new ArrayDeque<>(Arrays.asList(root));
+
+    while (!q.isEmpty()) {
+      int max = Integer.MIN_VALUE;
+      for (int sz = q.size(); sz > 0; --sz) {
+        TreeNode node = q.poll();
+        max = Math.max(max, node.val);
+        if (node.left != null)
+          q.offer(node.left);
+        if (node.right != null)
+          q.offer(node.right);
+      }
+      ans.add(max);
+    }
+
+    return ans;
+  }
+}
